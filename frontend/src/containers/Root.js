0ffb53efade28ece37705750f7f4b4223bc02d0e
asdf <tehasdf@gmail.com>
2015-12-16 09:12:55 +0100

    add frontend skeleton

---
+++ frontend/src/containers/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Root = props => (
+    <div>
+        dupa
+    </div>
+);
+
+export default Root;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: {
+        main: "./src/main.js"
+    },
+    output: {
+        path: './dist',
+        filename: "[name].bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel?presets[]=es2015&presets[]=react&presets[]=stage-0"}
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
