79844fac57261a47bf45505d36c07a7ae5cf2fc6
asdf <tehasdf@gmail.com>
2015-12-16 07:59:57 +0100

    models..

--- api/maps_api/maps/models.py
+++ api/maps_api/maps/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+
+
+class Map(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+
+
+class MapFeature(models.Model):
+    FEATURE_TYPES = [
+        (1, 'Reveal')
+    ]
+
+    feature_type = models.SmallIntegerField(choices=FEATURE_TYPES)
+    map = models.ForeignKey(to=Map)
+    data = models.BinaryField()
