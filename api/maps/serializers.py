ed30e1843b175e34b83c46ef3583dd94959b379a
asdf <tehasdf@gmail.com>
2015-12-16 09:04:07 +0100

    mapview writing by view id tests

--- api/maps/serializers.py
+++ api/maps/serializers.py
@@ -6,6 +6,8 @@ from .models import Map, MapFeature
 class MapSerializer(serializers.ModelSerializer):
     class Meta:
         model = Map
+        fields = '__all__'
+
 
 
 class MapFeatureSerializer(serializers.ModelSerializer):
--- api/maps/tests/test_views.py
+++ api/maps/tests/test_views.py
@@ -1,3 +1,4 @@
+import json
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
@@ -5,7 +6,7 @@ from maps.models import Map
 from maps.views import MapViewSet
 
 
-class TestMapViewSet(TestCase):
+class TestMapRetrieving(TestCase):
     def setUp(self):
         self.obj = Map(name='the map', edit_id='edit id', view_id='view id')
         self.obj.save()
@@ -18,4 +19,22 @@ class TestMapViewSet(TestCase):
     def test_get_by_view_id(self):
         response = self.client.get(reverse('maps-detail', kwargs={'id': self.obj.view_id}))
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'the map')
+
+
+class TestWriting(TestCase):
+    def setUp(self):
+        self.obj = Map(name='the map', edit_id='edit id', view_id='view id')
+        self.obj.save()
+
+    def test_update_by_edit_id(self):
+        response = self.client.patch(reverse('maps-detail', kwargs={'id': self.obj.edit_id}),
+            json.dumps({'name': 'foobar'}), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'foobar')
+
+    def test_update_by_view_id(self):
+        response = self.client.patch(reverse('maps-detail', kwargs={'id': self.obj.edit_id}),
+            json.dumps({'name': 'foobar'}), content_type='application/json')
+        self.assertEqual(response.status_code, 403)
         self.assertEqual(response.data['name'], 'the map')
