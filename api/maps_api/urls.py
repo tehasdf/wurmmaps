c2e40b8aad1822cd98495c98b7ddeffa72c8da7c
asdf <tehasdf@gmail.com>
2016-01-24 17:08:31 +0100

    remove django-admin urls

--- api/maps_api/urls.py
+++ api/maps_api/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url, include
-from django.contrib import admin
 from django.contrib.staticfiles.views import serve
 
 from rest_framework_nested.routers import SimpleRouter, NestedSimpleRouter
@@ -14,7 +13,6 @@ router.register('features', MapFeaturesViewSet, 'features')
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
-    url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^static/(?P<path>.*)$', serve)
 ]
